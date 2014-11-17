760beb8746abaeddbb460f986edc68eb25cc684e
Tanuj Bhojwani <tanuj.bhojwani@gmail.com>
2014-11-17 20:27:37 +0530

    Correcting stupidity

--- fbpic/urls.py
+++ fbpic/urls.py
@@ -12,5 +12,6 @@ urlpatterns = patterns('',
     (r'^accounts/', include('django_facebook.auth_urls')), #Don't add this line if you use django registration or userena for registration and auth.
     url(r'^$', 'fbpic.views.home', name='home'),
     url(r'^next$', 'fbpic.views.next', name='next'),
+    url(r'^tagger$', 'fbpic.views.tagger', name='tagger'),
     url(r'^wall_post$', 'fbpic.views.wall_post', name='wall_post'),
 )
