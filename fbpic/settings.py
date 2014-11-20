67540855590c493be11e1dd971fab4eedf44e108
Tanuj Bhojwani <tanuj.bhojwani@gmail.com>
2014-11-21 00:33:39 +0530

    last ditch effort at debug toolbar

--- fbpic/settings.py
+++ fbpic/settings.py
@@ -128,3 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+def show_toolbar(request):
+    return True
+SHOW_TOOLBAR_CALLBACK = show_toolbar
