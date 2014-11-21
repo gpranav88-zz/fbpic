5e49d097d7732b193fd7b8633b9549f1668a4d45
Tanuj Bhojwani <tanuj.bhojwani@gmail.com>
2014-11-21 09:40:14 +0530

    Add batcam to admin

--- batcam/admin.py
+++ batcam/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from batcam.models import MyCustomProfile
+from batcam.models import MyCustomProfile, BatCamPictureTag
 # Register your models here.
 
 admin.site.register(MyCustomProfile)
+admin.site.register(BatCamPictureTag)
