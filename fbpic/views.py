96f521e21e81d108c2b241feb108f44d70e65cad
Tanuj Bhojwani <tanuj.bhojwani@gmail.com>
2014-11-22 16:49:41 +0530

    updated

--- fbpic/views.py
+++ fbpic/views.py
@@ -73,7 +73,7 @@ def home(request, zone):
                                 1243,
                                 1244]
                 if maxi in list_of_stupid:
-                    while (maxi+1) not in list_of_stupid:
+                    while (maxi+1) in list_of_stupid:
                         maxi = maxi + 1
                 request.user.mycustomprofile.batcam_id = maxi + 1
                 request.user.mycustomprofile.save()
