e1b6b2b3ff8c6b2b70c26ca1d183cd322566352c
Tanuj Bhojwani <tanuj.bhojwani@gmail.com>
2014-11-26 14:41:12 +0530

    untameable day 3 in batcam

--- fbpic/views.py
+++ fbpic/views.py
@@ -300,12 +300,12 @@ def untameable_poster(request):
                 "I get knocked down, but I get up again, you're never gonna keep me down. Here's a sneak from #BacardiUntameableZone",
                 "Where there's a will, I'll forge my way.  #BacardiUntameableZone"]
 
-    list_of_filenames=[137,139,155,179,192,201,214,222,224,225,238,239,240,241,242,243,244,245,246,247,"248_2",248,249,250,251,252,253,254,255,256,257,258,259,260,261,262,263,264,265,266,267,268,269,270,271,272,273,274,276,277,278,279,280,281,282,283,284,285,287,288,289,290,291,292,293,294,295,296,297,298,299,300,302,304,305,306,307,308,309,312,313,314,315,316,317,318,319,320,322,323,324,325,326,327,328,330,331,332,333,334,335,336,337,338,"339_2",339,340,341,343,344,345,346,347,348,349,351,352,353,354,356,358,359,360,361,362,363,365,366,367,369,370,371,372,373,374,375,376,377,378,379,380,381,382,384,385,386,387,390,393,394,395,413,415,417,418,419,420]
+    list_of_filenames=[2214,2255,2458]
     duser = "a"
     a=[]
     for current_filename in list_of_filenames:
         current_id=int(str(current_filename).split("_")[0])
-        current_user = MyCustomProfile.objects.get(untameable_id__exact=current_id)
+        current_user = MyCustomProfile.objects.get(batcam_id__exact=current_id)
         duser = current_user.user
         fb = duser.get_offline_graph()
         picture="http://batcam.bacardiindia.in/"+"static/fbpic/images/untameable/dump/day3-ut-names/"+str(current_filename)+".jpg"
