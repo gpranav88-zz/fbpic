81def873140f03cd926163b67d7adb9cb964a416
Tanuj Bhojwani <tanuj.bhojwani@gmail.com>
2015-01-15 12:25:31 +0530

    importing json

--- fbpic/views.py
+++ fbpic/views.py
@@ -14,6 +14,7 @@ from batcam.models import BatCamPictureTag, MyCustomProfile
 from open_facebook.api import OpenFacebook
 from django.db.models import Max
 from django.db.models import F
+import json
 import pickle
 import random
 import httplib, urllib, urllib2
